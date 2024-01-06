505c34849f34ab3234e456fb47c863496e7bc799
Charon <charon.s.tom@gmail.com>
2024-01-06 21:05:40 +0100

    implementing the first slider

--- components/SliderPresentation.tsx
+++ components/SliderPresentation.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper/modules";
@@ -5,8 +7,36 @@ import { Pagination, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { useState } from "react";
 
 export default function SliderPresentation() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const handleSlideChange = (swiper) => {
+    setCurrentSlide(swiper.activeIndex);
+  };
+
+  const slidesData = [
+    {
+      image: "/images/slider1.jpg",
+      title: "Le jardin",
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, magni maiores! Corrupti, eos quibusdam? Laborum pariatur est alias sit nam commodi accusantium laboriosam quis delectus voluptate architecto numquam, iure repudiandae.",
+    },
+    {
+      image: "/images/slider2.jpg",
+      title: "Le jardin",
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, magni maiores! Corrupti, eos quibusdam? Laborum pariatur est alias sit nam commodi accusantium laboriosam quis delectus voluptate architecto numquam, iure repudiandae.",
+    },
+    {
+      image: "/images/slider3.jpg",
+      title: "Le jardin",
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, magni maiores! Corrupti, eos quibusdam? Laborum pariatur est alias sit nam commodi accusantium laboriosam quis delectus voluptate architecto numquam, iure repudiandae.",
+    },
+  ];
+
   return (
     <div className="p-5">
       <Swiper
@@ -16,61 +46,41 @@ export default function SliderPresentation() {
         pagination={{ clickable: true }}
         speed={500}
         modules={[Pagination, Navigation]}
+        onSlideChange={handleSlideChange}
       >
-        <SwiperSlide>
-          <Image
-            src="/images/slider1.jpg"
-            alt="slider"
-            fill
-            style={{ objectFit: "cover" }}
-            className="rounded-xl"
-          />
-          <div className="absolute bottom-5 left-5 slide-in-from-bottom">
-            <h2 className="text-[100px]">Le jardin</h2>
-            <p className="">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure,
-              magni maiores! Corrupti, eos quibusdam? Laborum pariatur est alias
-              sit nam commodi accusantium laboriosam quis delectus voluptate
-              architecto numquam, iure repudiandae.
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image
-            src="/images/slider2.jpg"
-            alt="slider"
-            fill
-            style={{ objectFit: "cover" }}
-            className="rounded-xl"
-          />
-          <div className="absolute bottom-5 left-5 slide-in-from-bottom">
-            <h2 className="text-[100px]">Le jardin</h2>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure,
-              magni maiores! Corrupti, eos quibusdam? Laborum pariatur est alias
-              sit nam commodi accusantium laboriosam quis delectus voluptate
-              architecto numquam, iure repudiandae.
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image
-            src="/images/slider3.jpg"
-            alt="slider"
-            fill
-            style={{ objectFit: "cover" }}
-            className="rounded-xl"
-          />
-          <div className="absolute bottom-5 left-5">
-            <h2 className="text-[100px]">Le jardin</h2>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure,
-              magni maiores! Corrupti, eos quibusdam? Laborum pariatur est alias
-              sit nam commodi accusantium laboriosam quis delectus voluptate
-              architecto numquam, iure repudiandae.
-            </p>
-          </div>
-        </SwiperSlide>
+        {slidesData.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <Image
+              src={slide.image}
+              alt="slider"
+              fill
+              style={{ objectFit: "cover" }}
+              className="rounded-xl"
+            />
+            <div className="relative h-full text-white px-8 flex flex-col justify-around">
+              <h2 className="text-[100px]">{slide.title}</h2>
+
+              <div
+                className={`text-container ${
+                  currentSlide === index ? "text-show" : ""
+                }`}
+              >
+                <div
+                  className={`line-anim mb-8 ${
+                    currentSlide === index ? "line-show" : ""
+                  }`}
+                ></div>
+                <div
+                  className={`slide-in-from-top ${
+                    currentSlide === index ? "show" : ""
+                  }`}
+                >
+                  <p className="w-[40%]">{slide.content}</p>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
