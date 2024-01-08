0bb233aff89d64cda1fea858298198c3a9bc679b
Charon <charon.s.tom@gmail.com>
2024-01-08 22:45:16 +0100

    reviews part

--- components/ReviewCard.tsx
+++ components/ReviewCard.tsx
@@ -1,31 +1,33 @@
+import Image from "next/image";
 import React from "react";
 import { RiStarSFill } from "react-icons/ri";
 
-function ReviewCard() {
+function ReviewCard({ name, text, date, logo, starColor }) {
   return (
-    <div className="w-full bg-gray-200 rounded-lg p-4 relative">
-      <div className="bg-red-500 h-2 w-2 absolute top-4 right-4"></div>
+    <div className="w-full h-[300px] bg-gray-200 rounded-lg p-4 relative">
+      <Image
+        src={logo}
+        alt="logo"
+        width={25}
+        height={25}
+        className="absolute top-4 right-4 rounded-lg"
+      />
       <div className="flex gap-x-4 mb-2">
         <div className="h-14 w-14 rounded-full bg-red-200"></div>
         <div>
-          <div>Nom et Prénom</div>
-          <div className="text-sm">date</div>
+          <div>{name}</div>
+          <div className="text-sm">{date}</div>
         </div>
       </div>
       <div className="flex mb-2">
-        <RiStarSFill />
-        <RiStarSFill />
-        <RiStarSFill />
-        <RiStarSFill />
-        <RiStarSFill />
+        <RiStarSFill style={{ color: starColor }} />
+        <RiStarSFill style={{ color: starColor }} />
+        <RiStarSFill style={{ color: starColor }} />
+        <RiStarSFill style={{ color: starColor }} />
+        <RiStarSFill style={{ color: starColor }} />
       </div>
       <div className="">
-        <p className="text-sm">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores at
-          velit debitis. Vero omnis ratione velit harum molestias officiis
-          voluptates quibusdam fugiat repellendus recusandae, consequatur,
-          reiciendis mollitia dolorum assumenda! Hic?
-        </p>
+        <p className="text-xs">{text}</p>
       </div>
     </div>
   );
--- components/SliderReview.tsx
+++ components/SliderReview.tsx
@@ -4,10 +4,10 @@ import React, { useState } from "react";
 import ReviewCard from "./ReviewCard";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation, FreeMode } from "swiper/modules";
-
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/autoplay";
+import { data_airbnb, data_home } from "@/utils/review-data";
 import { RiStarSFill } from "react-icons/ri";
 import { NavigationOptions } from "swiper/types";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
@@ -23,63 +23,116 @@ function SliderReview() {
   };
 
   return (
-    <div className="flex h-[300px] m-4 p-4">
-      <div className="text-center md:p-20 flex-center flex-col w-1/4">
-        <div>Excellent</div>
-        <div className="flex mb-2">
-          <RiStarSFill />
-          <RiStarSFill />
-          <RiStarSFill />
-          <RiStarSFill />
-          <RiStarSFill />
+    <>
+      {/* Airbnb */}
+      <div className="flex h-[300px] m-4 p-4">
+        <div className="text-center md:p-20 flex-center flex-col w-1/4">
+          <div>Excellent</div>
+          <div className="flex mb-2 text-red-400 text-[30px]">
+            <RiStarSFill />
+            <RiStarSFill />
+            <RiStarSFill />
+            <RiStarSFill />
+            <RiStarSFill />
+          </div>
+          <div>basé sur 12 avis</div>
+          {/* <Image  /> */}
         </div>
-        <div>basé sur 40 avis</div>
-        {/* <Image  /> */}
+        <div className="slider-review w-3/4 py-4 px-4 relative ">
+          <Swiper
+            grabCursor={true}
+            autoplay
+            spaceBetween={20}
+            slidesPerView={"auto"}
+            navigation={
+              {
+                nextEl: ".swiper-button-nextu",
+                prevEl: ".swiper-button-prevu",
+                clickable: true,
+              } as NavigationOptions
+            }
+            modules={[Autoplay, Navigation]}
+            onSlideChange={handleSlideChange}
+          >
+            {data_airbnb.map((data, index) => (
+              <SwiperSlide key={index}>
+                <ReviewCard
+                  name={data.name}
+                  text={data.text}
+                  date={data.date}
+                  logo="/images/logoAirbnb.png"
+                  starColor="rgb(248 113 113)"
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <FaAngleLeft
+            className={`swiper-button-prevu -translate-y-1/2 z-20 text-[40px] text-primary-pink absolute top-1/2 -left-5 cursor-pointer ${
+              isPrevDisabled ? "opacity-50 " : ""
+            }`}
+          />
+          <FaAngleRight
+            className={`swiper-button-nextu -translate-y-1/2 z-20 text-[40px] text-primary-pink absolute top-[50%] -right-5 cursor-pointer ${
+              isNextDisabled ? "opacity-50 " : ""
+            }`}
+          />
+        </div>
       </div>
-      <div className="slider-review w-3/4 py-4 px-4 relative ">
-        <Swiper
-          grabCursor={true}
-          autoplay
-          spaceBetween={20}
-          slidesPerView={"auto"}
-          navigation={
-            {
-              nextEl: ".swiper-button-nextu",
-              prevEl: ".swiper-button-prevu",
-              clickable: true,
-            } as NavigationOptions
-          }
-          modules={[Autoplay, Navigation]}
-          onSlideChange={handleSlideChange}
-        >
-          <SwiperSlide className="">
-            <ReviewCard />
-          </SwiperSlide>
-          <SwiperSlide className="">
-            <ReviewCard />
-          </SwiperSlide>
-          <SwiperSlide className="">
-            <ReviewCard />
-          </SwiperSlide>
-          <SwiperSlide className="">
-            <ReviewCard />
-          </SwiperSlide>
-          <SwiperSlide className="">
-            <ReviewCard />
-          </SwiperSlide>
-        </Swiper>
-        <FaAngleLeft
-          className={`swiper-button-prevu -translate-y-1/2 z-20 text-[40px] text-primary-pink absolute top-1/2 -left-5 cursor-pointer ${
-            isPrevDisabled ? "opacity-50 " : ""
-          }`}
-        />
-        <FaAngleRight
-          className={`swiper-button-nextu -translate-y-1/2 z-20 text-[40px] text-primary-pink absolute top-[50%] -right-5 cursor-pointer ${
-            isNextDisabled ? "opacity-50 " : ""
-          }`}
-        />
+      {/* HomeExchange */}
+      <div className="flex h-[300px] m-4 p-4">
+        <div className="text-center md:p-20 flex-center flex-col w-1/4">
+          <div>Excellent</div>
+          <div className="flex mb-2 text-orange-500 text-[30px]">
+            <RiStarSFill />
+            <RiStarSFill />
+            <RiStarSFill />
+            <RiStarSFill />
+            <RiStarSFill />
+          </div>
+          <div>basé sur 8 avis</div>
+          {/* <Image  /> */}
+        </div>
+        <div className="slider-review w-3/4 py-4 px-4 relative ">
+          <Swiper
+            grabCursor={true}
+            autoplay
+            spaceBetween={20}
+            slidesPerView={"auto"}
+            navigation={
+              {
+                nextEl: ".swiper-button-nextu",
+                prevEl: ".swiper-button-prevu",
+                clickable: true,
+              } as NavigationOptions
+            }
+            modules={[Autoplay, Navigation]}
+            onSlideChange={handleSlideChange}
+          >
+            {data_home.map((data, index) => (
+              <SwiperSlide key={index}>
+                <ReviewCard
+                  name={data.name}
+                  text={data.text}
+                  date={data.date}
+                  logo="/images/logoHome.png"
+                  starColor="orange"
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <FaAngleLeft
+            className={`swiper-button-prevu -translate-y-1/2 z-20 text-[40px] text-primary-pink absolute top-1/2 -left-5 cursor-pointer ${
+              isPrevDisabled ? "opacity-50 " : ""
+            }`}
+          />
+          <FaAngleRight
+            className={`swiper-button-nextu -translate-y-1/2 z-20 text-[40px] text-primary-pink absolute top-[50%] -right-5 cursor-pointer ${
+              isNextDisabled ? "opacity-50 " : ""
+            }`}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
