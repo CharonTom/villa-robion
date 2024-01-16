7e7ebb77094f5e6df4007e2be0d2ef1e4bb0f2e4
Charon <charon.s.tom@gmail.com>
2024-01-16 18:20:33 +0100

    making the house visit page

--- components/sections/VisitVideo.tsx
+++ components/sections/VisitVideo.tsx
@@ -2,13 +2,17 @@ import React from "react";
 
 function VisitVideo() {
   return (
-    <section className="h-screen w-full bg-red-200">
-      {/* <video
-        controls
-        className="w-[90%] h-[90%] object-cover brightness-75  mx-auto"
-      >
-        <source src="/visitVideo.mp4" type="video/mp4" />
-      </video> */}
+    <section className="h-screen w-full">
+      <div className="w-[85%] h-[85%] p-4 mx-auto">
+        <iframe
+          className="h-full w-full rounded-xl"
+          src="https://www.youtube.com/embed/xAbgeOZvD1M?si=DYQT1w0wN0je_TYl"
+          title="YouTube video player"
+          frameorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
     </section>
   );
 }
--- components/sliders/SliderVisit.tsx
+++ components/sliders/SliderVisit.tsx
@@ -1,15 +1,54 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
 
 function SliderVisit({ images }: { images: string[] }) {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
   return (
     <div>
       <Swiper
-        spaceBetween={15}
-        className="w-[80%] h-[400px] rounded-xl bg-black"
+        loop={true}
+        spaceBetween={10}
+        navigation={true}
+        thumbs={{
+          swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
+        }}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="h-[400px] rounded-xl bg-black"
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <div className="flex-center h-full w-full">
+              <Image
+                alt={`Image ${index + 1}`}
+                src={`/images/VisitSlider/${image}`}
+                fill
+                className="block h-full w-full object-cover"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* Thumbnail */}
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        loop={true}
+        spaceBetween={12}
+        slidesPerView={4}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="thumbs mt-3 h-32 rounded-lg"
       >
         {images.map((image, index) => (
           <SwiperSlide key={index}>
