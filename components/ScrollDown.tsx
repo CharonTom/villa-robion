0ad1e610780c01d7f8bcce43eb179b742515413c
Charon <charon.s.tom@gmail.com>
2024-01-23 19:05:06 +0100

    optimizing the SEO

--- components/ScrollDown.tsx
+++ components/ScrollDown.tsx
@@ -8,8 +8,9 @@ function ScrollDown() {
       <Link
         smooth={true}
         to="presentation"
+        href="#presentation"
         duration={500}
-        className="cursor-pointer group absolute bottom-3 right-1/2 translate-x-1/2 text-white flex-center flex-col py-3 "
+        className="group absolute bottom-3 right-1/2 translate-x-1/2 text-white flex-center flex-col py-3 "
       >
         <p className="[text-shadow:_3px_3px_3px_rgb(0_0_0_/_70%)]">
           scroll down
